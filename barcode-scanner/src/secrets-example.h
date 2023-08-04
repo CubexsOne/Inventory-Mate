4d1832791f5e1e66e9182e342f08a6155fa02ab8
Pascal Sauer <email@cubexs.dev>
2023-08-04 22:01:28 +0200

    Add example file for secrets

---
+++ barcode-scanner/src/secrets-example.h
@@ -0,0 +1,13 @@
+// secrets.h
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+#define MQTT_HOST ""
+#define MQTT_USER ""
+#define MQTT_PASSWORD ""
+#define MQTT_READ_TOPIC ""
+#define MQTT_WRITE_TOPIC ""
+
+#endif
