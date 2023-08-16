ccb9e01285272be0e7f3508bec0f4c57d0f7fa83
Pascal Sauer <email@cubexs.dev>
2023-08-16 21:34:01 +0200

    Use Scanner

--- barcode-scanner/src/main.cpp
+++ barcode-scanner/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "DisplayManager.h"
 #include "MqttClient.h"
+#include "Scanner.h"
 #include "WifiManager.h"
 
 // Display
@@ -9,6 +10,9 @@ DisplayManager displayManager;
 // MQTT
 MqttClient mqttClient;
 
+// Scanner
+Scanner scanner;
+
 // Wifi
 WifiManager wifiManager;
 
@@ -16,10 +20,12 @@ void setup() {
   Serial.begin(9600);
   displayManager.begin();
   mqttClient.begin(displayManager);
+  scanner.begin(mqttClient);
 }
 
 void loop() {
   wifiManager.loop();
   displayManager.loop();
   mqttClient.loop();
+  scanner.loop();
 }
