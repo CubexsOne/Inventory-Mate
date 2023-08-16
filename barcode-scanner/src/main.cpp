76245cb5aeee9708bcaaea9b6e91228a4da67f88
Pascal Sauer <email@cubexs.dev>
2023-08-16 19:03:47 +0200

    Use DisplayManager

--- barcode-scanner/src/main.cpp
+++ barcode-scanner/src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
-#include "WifiManager.h"
+#include "DisplayManager.h"
 #include "MqttClient.h"
+#include "WifiManager.h"
 
-// Wifi
-WifiManager wifiManager;
+// Display
+DisplayManager displayManager;
 
 // MQTT
 MqttClient mqttClient;
 
+// Wifi
+WifiManager wifiManager;
+
 void setup() {
   Serial.begin(9600);
+  displayManager.begin();
+  mqttClient.begin();
 }
 
 void loop() {
   wifiManager.loop();
+  displayManager.loop();
   mqttClient.loop();
 }
